e054b90db1d5d63b7acc1609d16a50ff647cf2f5
Tanner Bebb <17tbebb@gmail.com>
2024-02-21 09:44:43 -0800

    Resetting to try and allow user input for the file stream process.

--- Ifstream and Ofstream/Main.cpp
+++ Ifstream and Ofstream/Main.cpp
@@ -1,32 +1,18 @@
-/*Practicing using the ifstream and the ofstream*/
+/*Practicing using the ifstream and the ofstream
+
+NOTES:
+It seems like ofstream and ifstream are used as data types.
+*/
 
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
-
 int main() {
-	//Creates a new string that will get the text from the file.
-	string myText;
 
-	//This should read the text from the file that is specified.
-	ifstream MyReadFile("testFile.txt");
-
-	//While loop that will use the getline function to output all of the text stored in that file.
-	while (getline(MyReadFile, myText)) {
-		cout << myText;
-	}
-
-	MyReadFile.close();
-	////This will create and open the file.
-	//ofstream myFile("testFile.txt");
-
-	////This will write to the file.
-	//myFile << "This is should write to the txt file.";
-
-	////This will close the file.
-	//myFile.close();
+	//Declaring a variable of type ofstream and calling it outputTest.
+	ofstream outputTest;
 
 	return 0;
 }
