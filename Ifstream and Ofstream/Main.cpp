548c7ef163575a343433a8b88b105de6499448e3
Tanner Bebb <17tbebb@gmail.com>
2024-02-21 09:17:20 -0800

    Testing to see if I can get the file to work correctly.

--- Ifstream and Ofstream/Main.cpp
+++ Ifstream and Ofstream/Main.cpp
@@ -1,10 +1,20 @@
 /*Practicing using the ifstream and the ofstream*/
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 
 int main() {
+
+	//This will create and open the file.
+	ofstream myFile("testFile.txt");
+
+	//This will write to the file.
+	myFile << "This is should write to the txt file.";
+
+	//This will close the file.
+	myFile.close();
 
 	return 0;
 }
