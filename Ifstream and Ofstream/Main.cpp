b3fc860f9c66b7e2af8727c8965f5dba048918fa
Tanner Bebb <17tbebb@gmail.com>
2024-02-29 09:27:25 -0800

    Finished all of the items on the todo list. I am unsure as to what I want to do with this next.

--- Ifstream and Ofstream/Main.cpp
+++ Ifstream and Ofstream/Main.cpp
@@ -5,12 +5,12 @@ NOTES:
 	Finally figured out how to allow the user to write the text to a txt file.
 
 TODO:
-	Allow the user to give a name to the outputfile
+	Allow the user to give a name to the outputfile - FINISHED
 	Get user's first name. - FINISHED
 	Get user's last name. - FINISHED
-	Get user's middle name.
-	Get user's birth month.
-	Print all of that information to the screen.
+	Get user's middle name. - FINISHED
+	Get user's birth month. - FINISHED
+	Print all of that information to the screen. - FINISHED
 */
 
 #include <iostream>
@@ -19,42 +19,35 @@ TODO:
 using namespace std;
 
 int main() {
-	/*This will open the file that is listed in the parameters.*/
-	//ofstream outputTest("testfile.txt");
-
-	/*This will write to the file.*/
-	//outputTest << "This should write to the txt file.";
-
-	/*This closes the file that is currently opened.*/
-	//outputTest.close();
-
-	/*Delcared the variables myText and inputTest.*/
-	//string myText;
-	//ifstream inputTest("testfile.txt");
-
-	/*This is a while loop that should read each line and then print it out to the screen.*/
-	//while (getline(inputTest, myText)) {
-	//	cout << myText;
-	//}
 	
-	string firstName, lastName, birthMonth, middleName;
+	string firstName, lastName, birthMonth, middleName, fileName;
+
+	/*This will allow the user to input the name of the file.*/
+	cout << "Please enter the name of the file, then press the return key: " << endl;
+	cin >> fileName;
 
 	/*This is going to give a user a promot that will ask them to fill in their information.
-	Then that information will get stored in the declared variables.*/
+	Then that information will get stored into the declared variables.*/
 	cout << "Please enter your first name, then hit the return key: " << endl;
 	cin >> firstName;
 	cout << "Please enter your last name, then hit the return key: " << endl;
 	cin >> lastName;
+	cout << "Please enter your middle name, then hit the return key: " << endl;
+	cin >> middleName;
+	cout << "Please enter the month your were born in, then hit the return key: " << endl;
+	cin >> birthMonth;
 
 	/*This is going to declare the file that will store the user information.
 	The information is stored using the variables from above.*/
-	ofstream userInfo("user.txt");
+	ofstream userInfo(fileName + ".txt");
 	userInfo << firstName << endl;
+	userInfo << middleName << endl;
 	userInfo << lastName << endl;
+	userInfo << birthMonth << endl;
 
 	/*Declare a variable of type ifstream called getuserinfo.
 	Declared a variable of type string called mytext.*/
-	ifstream getUserInfo("user.txt");
+	ifstream getUserInfo(fileName + ".txt");
 	string myText;
 
 	/*While loop that will use the get line command to read through
